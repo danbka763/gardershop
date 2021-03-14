5d64060dec47657553bfc5d209498ce545fcfcee
Danil <48606033+danbka763@users.noreply.github.com>
2021-03-14 20:15:08 +0700

    start x2

--- js/pages.js
+++ js/pages.js
@@ -1,7 +1,38 @@
-// go page
+// переход на другую страницу
 function page(namePage) {
 
 }
+
+// Добавляем 
+document.getElementsByTagName("article")[0].innerHTML = 
+  `
+    <header>
+      <div id="logo">
+        <img src="PNG/logo.png" alt="logotype">
+        <div>
+          <h1>GARDERSHOP</h1>
+          <p>Панель администратора</p>
+        </div>
+      </div>
+      </header>
+
+      <aside>
+      <nav>
+        <a href="./" id="Homepage">
+          Главная
+        </a>
+        <a href="Orders" id="Orders">
+          Заказы
+        </a>
+        <a id="products">
+          Товары
+        </a>
+        <a id="shop">
+          Магазины
+        </a>
+      </nav>
+    </aside>
+  `
 
 const pages = ["Homepage", "Orders"]
 
@@ -10,7 +41,6 @@ let complete = false
 for (let i = 0; i < pages.length; i++) {
   if (window.location.href.indexOf(pages[i]) > -1) {
     complete = true
-
     console.log(document.getElementById(pages[i]))
   }
 }
